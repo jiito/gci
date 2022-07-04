22a85742b615b742d41a3c5f4f5bae09f25e45bd
jiito <benjamin.allanrahill@gmail.com>
2022-07-04 08:01:09 -0600

    git get branches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use git2::Repository;
+
 fn main() {
     println!("Hello, world!");
+    let repo = match Repository::open(".") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e)
+    };
+
+
+    let branches = match repo.branches(None) {
+        Ok(branches) => branches.filter_map(|x| x.ok()),
+        Err(e) => panic!("failed to get branches")
+    };
+
+    for branch in branches {
+        println!("{}", branch.0.name().unwrap().unwrap())
+    }
+
 }
